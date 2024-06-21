ef573f6d0622f4b044ea389ffae9076193a2940b
Jake Speyer <info@jakespeyer.dev>
2024-06-20 20:54:49 -0600

    updated to use api calls

--- compilation/sync_manifest.js
+++ compilation/sync_manifest.js
@@ -1,66 +1,62 @@
 /**
  * sync_manifest.js
- * 
+ *
  * Utilizes the CurseForge API to sync the mods folder with the manifest.json file.
  */
 
 /// File fetching service for reading in the manifest.json file.
-const fs = require('fs').promises;
-const env = require('dotenv').config();
+const fs = require('fs').promises
+const env = require('dotenv').config()
+const { Curseforge } = require('node-curseforge')
 
 // Access the API key from process.env
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.API_KEY
+
+const cf = new Curseforge(API_KEY)
+const mc = cf.get_game('minecraft')
 
 /// Directories for reference in rest of file.
-const MANIFEST_PATH = "../manifest.json";
-const MOD_PATH = "../mods/"
+const MANIFEST_PATH = '../manifest.json'
+const MOD_PATH = '../mods/'
 
 /**
  * Main function for the script.
  * This is executed at file entry- Async so we can await api calls when necessary.
  */
-async function main(){
-    /// Fetch the manifest.json file from the directory (this should be
-    /// the main working directory of the repository/ modpack)
-    const manifestFileJson = await readManifest(MANIFEST_PATH);
-    const manifestFileMods = manifestFileJson.files
-    const manifestFileGameVersion = manifestFileJson.minecraft.version
+async function main () {
+  /// Fetch the manifest.json file from the directory (this should be
+  /// the main working directory of the repository/ modpack)
+  const manifestFileJson = await readManifest(MANIFEST_PATH)
+  const manifestFileMods = manifestFileJson.files
+  const manifestFileGameVersion = manifestFileJson.minecraft.version
 
-    /// Loop through the manifest mods and attempt to download them from the manifest.json file.
-    for(const mod of manifestFileMods){
-        await checkModExists(manifestFileGameVersion, mod.projectID, mod.fileID)
-        console.log(mod.projectID)
-    }
-
+  /// Loop through the manifest mods and attempt to download them from the manifest.json file.
+  for (const mod of manifestFileMods) {
+    await checkModExists(manifestFileGameVersion, mod.projectID, mod.fileID)
+    console.log(mod.projectID)
+  }
 }
 
 /**
- * 
+ *
  */
-async function readManifest(path){
-    try{
-        const data = await fs.readFile(path, 'utf8');
-        const jsonObject = JSON.parse(data);
-        return jsonObject;
-    }catch(e){
-        console.error(`Error reading file: ${e}`);
-        throw e;
-    }
+async function readManifest (path) {
+  try {
+    const data = await fs.readFile(path, 'utf8')
+    const jsonObject = JSON.parse(data)
+    return jsonObject
+  } catch (e) {
+    console.error(`Error reading file: ${e}`)
+    throw e
+  }
 }
 
-
-async function checkModExists(version, mod, fileID){
-    const url = `https://api.curseforge.com/v1/mods/${mod}/files/${fileID}/download-url`
-    const headers = {
-        'Accept': 'application/type',
-        'X-Api-Key': API_KEY
-    }
-
-    const response = await fetch(url, headers);
-    console.log(response)
+async function checkModExists (version, mod, fileID) {
+  const file = await cf.get_file(mod, fileID)
+  console.log(file)
 }
 
-async function downloadModFromCurseForge(){}
+async function downloadModFromCurseForge () {}
 
 /// FILE ENTRY POINT
-main();
+main()
